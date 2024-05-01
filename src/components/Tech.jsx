140f6859159a64eca39687ab746b8eeca20a9326
Ajkce <kshettryajaya@gmail.com>
2024-05-01 21:33:26 +1000

    Tech stack

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
+import { styles } from "../style";
 
 const Tech = () => {
   return (
-    <div>Tech</div>
-  )
-}
+    <div className={`${styles.paddingX} mx-auto pt-[100px]`} id="work">
+      <p className={`${styles.sectionSubText} text-center`}>
+        What I expertise on
+      </p>
+      <h2 className={`${styles.sectionHeadText} text-center`}>
+       Tech Stack
+      </h2>
+    </div>
+  );
+};
 
-export default Tech
+export default Tech;

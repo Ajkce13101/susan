c52f68a35192ded971bc1d70e30eee511558b5cc
Ajaya K C <kshettryaj93@gmail.com>
2024-05-01 14:36:33 +1000

    3d object lighting fix

--- src/style.js
+++ src/style.js
@@ -4,7 +4,7 @@ const styles = {
   padding: "sm:px-16 px-6 sm:py-16 py-10",
 
   heroHeadText:
-    "font-black text-white xl:[text-60px] lg:text-[50px] sm:text-[40px] xs:text-[30px] text-[30px] lg:leading-[98px] mt-2",
+    "font-black text-white xl:[text-60px] lg:text-[50px] sm:text-[40px] xs:text-[35px] text-[35px] lg:leading-[98px] mt-2",
   heroSubText:
     "text-[#dfd9ff] font-medium lg:text-[18px] sm:text-[16px] xs:text-[16px] text-[16px] lg:leading-[40px]",
 

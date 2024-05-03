499e28ff10c21a8ce828c9efb0e3a39cbe2bbef8
Ajkce <kshettryaj13101@gmail.com>
2024-05-03 14:54:44 +1000

    Testing

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -13,22 +13,18 @@ const Ball = (props) => {
 
   return (
     <Float distance={0.5} speed={1}>
-      <ambientLight intensity={0.25} />
-      <directionalLight position={[0, 0, 0.05]} />
       <mesh castShadow receiveShadow scale={2.3}>
         <icosahedronGeometry args={[1, 1]} />
         <meshStandardMaterial
           color="#fff8eb"
           polygonOffset
           polygonOffsetFactor={-5}
-          flatShading
         />
         <Decal
           position={[0, 0, 1]}
           rotation={[2 * Math.PI, 0, 6.25]}
           scale={1}
           map={decal}
-          flatShading
         />
       </mesh>
     </Float>

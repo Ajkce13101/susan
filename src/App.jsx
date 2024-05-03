adae8cbf8d6cba66eb7f0a632136f15915d352e6
Ajkce <kshettryaj13101@gmail.com>
2024-05-03 14:32:53 +1000

    Testing

--- src/App.jsx
+++ src/App.jsx
@@ -22,8 +22,8 @@ const App = () => {
         </div>
         <About />
         <Experience />
-        {/* <Tech />
-        <Works />
+        <Tech />
+        {/* <Works />
         <Feedbacks />
         <div className="relative z-0">
           <Contact />

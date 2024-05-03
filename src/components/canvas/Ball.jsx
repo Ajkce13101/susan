4d70b078402966653bcc0874b516a217cef05cf2
Ajkce <kshettryaj13101@gmail.com>
2024-05-03 15:04:21 +1000

    Testing

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -7,19 +7,20 @@ import {
   Preload,
   useTexture,
 } from "@react-three/drei";
+import { Tooltip } from "react-tooltip";
+
+import CanvasLoader from "../Loader";
 
 const Ball = (props) => {
   const [decal] = useTexture([props.imgUrl]);
 
   return (
     <Float distance={0.5} speed={1}>
-      <mesh scale={2.3}>
+      <ambientLight intensity={0.25} />
+      <directionalLight position={[0, 0, 0.05]} />
+      <mesh castShadow receiveShadow scale={2.3}>
         <icosahedronGeometry args={[1, 1]} />
-        <meshStandardMaterial
-          color="#fff8eb"
-          polygonOffset
-          polygonOffsetFactor={-5}
-        />
+        <meshStandardMaterial color="#ffffff" flatShading roughness={0.5} />
         <Decal
           position={[0, 0, 1]}
           rotation={[2 * Math.PI, 0, 6.25]}
@@ -33,9 +34,11 @@ const Ball = (props) => {
 
 const BallCanvas = ({ icon }) => {
   return (
-    <Canvas gl={{ preserveDrawingBuffer: true }}>
+    <Canvas frameloop="always" gl={{ preserveDrawingBuffer: true }}>
       <OrbitControls enableZoom={false} />
       <Ball imgUrl={icon} />
+
+      <Preload all />
     </Canvas>
   );
 };

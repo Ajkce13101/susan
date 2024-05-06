49d7c1bf648cbbf7c8eaffa817245fad394a8d43
Ajkce <kshettryaj13101@gmail.com>
2024-05-06 14:36:23 +1000

    Tech skills section

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -4,17 +4,19 @@ import { BallCanvas } from "./canvas";
 import { technologies } from "../constants";
 import { Tooltip } from "react-tooltip";
 import "./ball.css";
+import VerifiedIcon from "@mui/icons-material/Verified";
 
 const Tech = () => {
   return (
-    <div className={`${styles.padding} mx-auto pt-[100px]`} id="skills">
+    <div
+      className={`${styles.paddingX} mx-auto pt-[200px] max-w-[1400px] pb-[100px]`}
+      id="skills"
+    >
       <p className={`${styles.sectionSubText} text-center`}>
         What I expertise on
       </p>
       <div className="flex flex-col items-center">
-        <h2 className={`${styles.sectionHeadText} text-center`}>
-          Tools & Technology
-        </h2>
+        <h2 className={`${styles.sectionHeadText} text-center`}>Skills</h2>
         <ul className="dottedLine ">
           <li></li>
           <li></li>
@@ -22,26 +24,68 @@ const Tech = () => {
         </ul>
       </div>
 
-      <div className="flex flex-row flex-wrap justify-center gap-10 mt-10">
-        <Tooltip
-          id="my-tooltip"
-          style={{ backgroundColor: "white", color: "black" }}
-        />
-        {technologies.map((tech) => {
-          return (
-            <a
-              data-tooltip-id="my-tooltip"
-              data-tooltip-content={tech.name}
-              data-tooltip-variant="info"
-            >
-              <div className="state">
-                <div className="w-20 h-20  rounded-full ball " key={tech.name}>
-                  <img src={tech.icon} className="p-4 z-10" alt="" />
-                </div>
-              </div>
-            </a>
-          );
-        })}
+      <div className="flex gap-[100px] flex-col xl:flex-row ">
+        <div className=" w-full h-full flex-1 bg-tertiary p-6 rounded-xl mt-10 shadow-card ">
+          <p className="blue-text-gradient font-extrabold text-2xl mb-10">
+            Proficiency:
+          </p>
+
+          <div>
+            <div className="flex gap-4 mb-4">
+              <VerifiedIcon color="primary"></VerifiedIcon>
+              <p>Ticketing Systems</p>
+            </div>
+            <div className="flex gap-4 mb-4">
+              <VerifiedIcon color="primary"></VerifiedIcon>
+              <p> Azure Active Directory (Azure AD)</p>
+            </div>
+            <div className="flex gap-4 mb-4">
+              <VerifiedIcon color="primary"></VerifiedIcon>
+              <p>Remote Desktop Software</p>
+            </div>
+            <div className="flex gap-4 mb-4">
+              <VerifiedIcon color="primary"></VerifiedIcon>
+              <p>Remote Access and VPN</p>
+            </div>
+            <div className="flex gap-4 mb-4">
+              <VerifiedIcon color="primary"></VerifiedIcon>
+              <p>Mobile Device Management (MDM) Solutions</p>
+            </div>
+          </div>
+        </div>
+        <div className=" mt-10 flex-1">
+          <p className="blue-text-gradient font-extrabold text-2xl mb-10">
+            Tools & Technologies:
+          </p>
+          <div className="flex flex-row flex-wrap justify-start gap-10">
+            <Tooltip
+              id="my-tooltip"
+              style={{ backgroundColor: "white", color: "black" }}
+            />
+            {technologies.map((tech) => {
+              return (
+                <a
+                  data-tooltip-id="my-tooltip"
+                  data-tooltip-content={tech.name}
+                  data-tooltip-variant="info"
+                >
+                  <div className="state">
+                    <div
+                      className="w-[80px] h-[80px] max-md:w-[70px] max-md:h-[70px] rounded-full ball "
+                      key={tech.name}
+                    >
+                      <img
+                        src={tech.icon}
+                        className="p-4 max-md:p-3 z-10"
+                        alt=""
+                      />
+                    </div>
+                  </div>
+                </a>
+              );
+            })}
+          </div>
+        </div>
       </div>
     </div>
   );
